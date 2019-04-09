9214f66e1804df23a99e72e960b7cb621a59a258
bayan-kawas <bayan.kawas99@gmail.com>
2019-04-09 19:22:09 +0200

    Playing with ints

---
+++ ___Experiments/example/script.js
@@ -0,0 +1,8 @@
+//convert number to string 
+let n1 = 100 ; 
+console.log(n1.toString() +5)
+let n2 = 300 ;
+console.log(n2 + "" + 55);
+//use math.abs to get the absolute values 
+let n3 = -200;
+console.log( Math.abs(n3));
