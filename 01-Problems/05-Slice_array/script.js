7db260d3bd60c026f81131ac620c0ddb9089f514
bayan-kawas <bayan.kawas99@gmail.com>
2019-04-27 15:59:33 +0200

    slice array

--- 01-Problems/05-Slice_array/script.js
+++ 01-Problems/05-Slice_array/script.js
@@ -8,7 +8,16 @@
 
 // Don't use the ready slice method, use loops to implement yours
 function slice(arr, start) {
-
+	if(start==0){
+		return arr;
+	}
+	if(start>=arr.length){
+		return [];
+	}
+	if(start < 0){
+		start=start + arr.length;//-2+6=4
+		//start=arr.length +(start)//6+(-2)=4
+	}
     let result = [];
     for (let i = start; i < arr.length; i++) {
         // result[result.length] = arr[i];
@@ -17,12 +26,10 @@ function slice(arr, start) {
     return result;
 }
 
-console.log(slice([1, 2, 3, 4], 0));
-//slice([1,2,3,4],0) === "1234"
 if (slice([1, 2, 3, 4], 0).toString() == [1, 2, 3, 4].toString()
-// && slice([1, 2, 3, 4], 10).toString() == [].toString()
-// && slice([1, 2, 3, 4], 2).toString() == [3, 4].toString()
-// && slice([1, 2, 3, 4, 5, 6], -2).toString() == [5, 6].toString()
+ && slice([1, 2, 3, 4], 10).toString() == [].toString()
+ && slice([1, 2, 3, 4], 2).toString() == [3, 4].toString()
+ && slice([1, 2, 3, 4, 5, 6], -2).toString() == [5, 6].toString()
 ) {
     console.log('Your code passed the tests');
 } else {
